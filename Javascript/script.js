9cf9d904deec76e69526a162373b9ddb13d90409
LaShonda Haywood <lofton_lashonda@yahoo.com>
2021-03-25 23:06:31 -0500

    Updated Webpage

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+var spaService = 200;
+var checkingAccount = 500;
+
+if (spaService <= checkingAccount); {
+    console.log ("Congratulations on your purchase!");
+} 
+else {
+    console.log ("Insufficient Funds! Please pick a different service.");
+}
